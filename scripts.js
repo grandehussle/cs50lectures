673bc9f4caaf95421497e4cfd2536b30b403496d
grandehussle <grande.maitre@gmail.com>
2019-05-18 01:35:19 +0300

    new code for the js script

---
+++ scripts.js
@@ -0,0 +1,19 @@
+
+    var word = 'This is ';
+    var word2 = ' a sentence';
+    var sentence = word + word2;
+    document.write(sentence)
+    num1 = 5;
+    num2 = 5;
+    total = num1 / num2;
+    document.write(total);
+    alpha = "ABCEFGHIJLKMNOP";
+    str_length = alpha.length;
+    document.write(str_length);
+    result = alpha.substring(0,5);
+    document.write(result);
+    a = ['Cat ', 'Dog ', 'Crab ', 88, true];
+    document.write(a[4])
+    function sayHello(who) {
+        document.write("Hello, " + who)
+    }
